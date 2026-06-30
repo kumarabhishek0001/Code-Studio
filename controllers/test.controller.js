d3ed020510259d46cc43b163b1ab864c297d2383
Abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2026-06-30 15:03:06 +0530

    FEATURE: add morgan and chalk

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -1,5 +1,6 @@
 const testRouteController = (req, res) => {
     console.log("Test route successfull")
+    res.send("test route is working!!")
 }
 
 export default testRouteController;
--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// DEPENDENCIES
+import chalk from "chalk";
+import morgan from "morgan";
+
 import express from "express";
 const app = express();
 const port = 3000
@@ -5,6 +9,7 @@ const port = 3000
 import testRoute from "./routes/test.route.js"
 
 app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 app.get("/", (req, res) => {
     res.send("Server is live");
@@ -13,5 +18,5 @@ app.get("/", (req, res) => {
 app.use('/api/v1/test',testRoute)
 
 app.listen(port, () => {
-    console.log("server live on port 3000")
+    console.log("Server live on", chalk.bgWhiteBright(`http://localhost:3000`))
 })
