770d80dca9f45885922bbdb9f88cdc17f1d99bc0
Abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2026-06-30 18:02:31 +0530

    FEATURE: add user login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,23 +1,24 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import chalk from "chalk";
 
 // PRISMA CLINET IMPORT
-import {prisma} from "../db/prisma.js";
+import { prisma } from "../db/prisma.js";
+import { error } from "node:console";
 
 const createUserController = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    if(!username || !email || !password){
+    if (!username || !email || !password) {
       return res.status(400).json({
         error: "BAD REQUEST",
-        message: "all fields are required"
-      })
+        message: "all fields are required",
+      });
     }
 
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt)
-
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const response = await prisma.Users.create({
       data: {
@@ -37,11 +38,56 @@ const createUserController = async (req, res) => {
       message: error.message,
     });
   }
-}
+};
 
+const loginUserController = async (req, res) => {
+  // todo: here i am searching using email first then checking password, instead make this a index and check performance
+  const { email, password } = req.body;
 
-const loginUserController = async(req, res) => {
+  if (!email || !password) {
+    return res.status(400).json({
+      error: "BAD REQUEST",
+      message: "all fields are mandatory",
+    });
+  }
 
-}
+  const response = await prisma.Users.findUnique({
+    where: {
+      email: email,
+    },
+  });
 
-export {createUserController, loginUserController}
+  if (!response) {
+    return res.status(401).json({
+      error: "UNAUTHORIZED",
+      message: "Invalid credentials",
+    });
+  }
+
+  // console.log(chalk.blue(JSON.stringify(response)))
+  const hashedPassword = response.password;
+  const checkPassword = await bcrypt.compare( password, hashedPassword );
+
+  if (!checkPassword) {
+    return res.status(401).json({
+      error: "UNAUTHORIZED",
+      message: "Invalid credentials/password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: response.id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1d",
+    },
+  );
+
+  return res.status(200).json({
+    token
+  });
+};
+
+export { createUserController, loginUserController };
