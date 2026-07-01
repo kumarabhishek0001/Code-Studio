0d4566f8f42b39a52ba48abca7e4e3b0fadcbd72
Abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2026-07-02 02:33:17 +0530

    FEATURE: add get question by ID

--- controllers/question.controller.js
+++ controllers/question.controller.js
@@ -1,5 +1,6 @@
 import { convertToObject } from "typescript";
 import { prisma } from "../db/prisma.js";
+import chalk from "chalk";
 
 const createQuestion = async (req, res) => {
   try {
@@ -23,27 +24,66 @@ const createQuestion = async (req, res) => {
       message: "question created",
       question: response,
     });
-  } catch(error) {
-    console.log("create-problem-controller issue", error)
+  } catch (error) {
+    console.log("create-problem-controller issue", error);
     return res.status(500).json({
-        message: "internal server error"
-    })
+      message: "internal server error",
+    });
   }
 };
 
 const getAllQuestionController = async (req, res) => {
   try {
-    const response = await prisma.Problems.findMany()
+    const response = await prisma.Problems.findMany();
     res.status(200).json({
       message: "fetched all questions",
-      questions: response
-    })
+      questions: response,
+    });
   } catch (error) {
-    console.log("problem getallquestioncontroller")
+    console.log("problem getallquestioncontroller");
     res.status(500).json({
-      message: "Internal server error"
-    })
+      message: "Internal server error",
+    });
   }
-}
+};
 
-export { createQuestion, getAllQuestionController };
+const getQuestionByID = async (req, res) => {
+  try {
+    const questionId = Number(req.params.questionId);
+    // console.log(chalk.red(questionId));
+
+    const question = await prisma.problems.findUnique({
+      where: {
+        id: questionId,
+      },
+    });
+
+    if (!question) {
+      return res.status(404).json({
+        message: "Question not found",
+      });
+    }
+    // console.log(chalk.red("question"));
+    // console.log(question);
+
+    const { id: questionNumber, title, description, difficulty } = question;
+
+    return res.status(200).json({
+      message: "Question found",
+      question: {
+        questionNumber,
+        title,
+        description,
+        difficulty,
+      },
+    });
+  } catch (err) {
+    console.log("getQuestionByID controller error:", err);
+    return res.status(500).json({
+      message: "internal server error",
+    });
+  }
+};
+
+
+export { createQuestion, getAllQuestionController, getQuestionByID };
--- routes/qestion.route.js
+++ routes/qestion.route.js
@@ -1,10 +1,15 @@
-import { createQuestion, getAllQuestionController } from "../controllers/question.controller.js";
+import {
+  createQuestion,
+  getAllQuestionController,
+  getQuestionByID,
+} from "../controllers/question.controller.js";
 import { Router } from "express";
 import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/create-question", authMiddleware, createQuestion);
-router.get("/get-all-questions", authMiddleware, getAllQuestionController)
+router.get("/get-all-questions", authMiddleware, getAllQuestionController);
+router.get("/get-question/:questionId", authMiddleware, getQuestionByID)
 
 export default router;
